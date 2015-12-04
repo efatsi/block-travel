efatsi/block-travel

commit 81ed8bf28e07e126a67bc541cf80dfee413359f7

Determining position of multiple cursors independently

lib/block-travel.coffee
@@ -1,37 +1,39 @@
 blockTravel = (editor, direction, select) ->
   up        = direction == "up"
   lineCount = editor.getScreenLineCount()
-  row       = editor.getCursorScreenPosition().row
-  count     = 0
 
-  loop
-    count += 1
+  editor.getCursors().forEach (cursor) ->
+    row   = cursor.getBufferRow()
+    count = 0
 
-    if up
-      rowIndex = row - count
+    loop
+      count += 1
+
+      if up
+        rowIndex = row - count
+      else
+        rowIndex = row + count
+
+      if rowIndex < 0
+        count = row
+        break
+      else if rowIndex >= lineCount
+        count = lineCount - row
+        break
+
+      if editor.lineTextForScreenRow(rowIndex).replace(/^\s+|\s+$/g, "") is ""
+        break
+
+    if select
+      if up
+        cursor.selection.selectUp(count)
+      else
+        cursor.selection.selectDown(count)
     else
-      rowIndex = row + count
-
-    if rowIndex < 0
-      count = row
-      break
-    else if rowIndex >= lineCount
-      count = lineCount - row
-      break
-
-    if editor.lineTextForScreenRow(rowIndex).replace(/^\s+|\s+$/g, "") is ""
-      break
-
-  if select
-    if up
-      editor.selectUp(count)
-    else
-      editor.selectDown(count)
-  else
-    if up
-      editor.moveUp(count)
-    else
-      editor.moveDown(count)
+      if up
+        cursor.moveUp(count)
+      else
+        cursor.moveDown(count)
 
 module.exports =
   activate: ->

spec/block-travel-spec.coffee
@@ -39,7 +39,7 @@ describe "BlockTravel", ->
       beforeEach ->
         waitsForPromise ->
           atom.workspace.open()
-        
+
         editor = atom.workspace.getActiveTextEditor()
         editor.setText """
           var quicksort = function () {
@@ -68,3 +68,24 @@ describe "BlockTravel", ->
         blockTravel(editor, "down", false)
         expect(editor.getCursorBufferPosition()).toEqual([9, 0])
         expect(editor.getCursorScreenPosition()).toEqual([6, 0])
+
+    describe "with multiple cursors", ->
+      beforeEach ->
+        waitsForPromise ->
+          atom.workspace.open()
+
+        editor = atom.workspace.getActiveTextEditor()
+        editor.setText """
+          console.log("Hello World");
+          console.log("Hello World");
+          console.log("Hello World");
+
+
+        """
+
+      it "properly moves each cursor independently", ->
+        editor.setCursorBufferPosition([0, 0])
+        editor.addSelectionBelow()
+        blockTravel(editor, "down", false)
+        expect(editor.getCursorBufferPosition()).toEqual([3, 0])
+        expect(editor.getCursors().length).toEqual(1)

commit 7d5059066cbe8e4fe733424ef8fc698103869bed

Use screen row.
Bug introduced in 81ed8bf28e07e126a67bc541cf80dfee413359f7

lib/block-travel.coffee
@@ -3,7 +3,7 @@ blockTravel = (editor, direction, select) ->
   lineCount = editor.getScreenLineCount()
 
   for cursor in editor.getCursors()
-    row   = cursor.getBufferRow()
+    row   = cursor.getScreenRow()
     count = 0
 
     loop